c7beb9d1a45648784d55ed5a0223ed884c1b6dec
Trevor Gerhardt <trevorgerhardt@gmail.com>
2013-05-27 11:06:34 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-component-ua
-============
 
-Wrapper of UrbanAirship's PhoneGap implementation for component(1)
+# component-ua
+
+  Wrapper of UrbanAirship's PhoneGap implementation for component(1)
+
+## Installation
+
+    $ component install mobqueue/component-ua
+
+## API
+
+   
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,108 @@
+
+/**
+ * Dependencies
+ */
+
+var Backbone = require('backbone')
+  , cordova = require('cordova-wrapper')
+
+/**
+ * Have push?
+ */
+
+var push = window.pushNotification;
+
+/**
+ * Expose `enable`
+ */
+
+module.exports.enable = enable;
+
+/**
+ * Expose `disable`
+ */
+
+module.exports.disable = disable;
+
+/**
+ * Register device id and tags
+ */
+
+function enable(id, tags) {
+  if (push) {        
+    // on register
+    push.registerEvent('registration', function(id) {
+      // check if push is enabled
+      push.isPushEnabled(function(enabled) {
+        if (!enabled) {
+          cordova.alert({
+            title: 'Push Notifications'
+          , message: 'Push notifications must be enabled for this app to work. Please check your settings.'
+          }, function() {
+            setTimeout(function() {
+              enable();
+            }, 1000);
+          });
+        }
+      });
+    });
+          
+    push.registerEvent('push', function(data) {
+      cordova.alert({
+        title: data.extras.title || 'Perfect'
+      , message: data.message
+      }, function() {
+        if (data.extras.url) {
+          Backbone.history.navigate(data.extras.url, { trigger: true });
+        }
+      });
+    });
+
+    push.getIncoming(function(data) {
+      if (data.message) {
+        cordova.alert({
+          title: data.extras.title || 'Perfect'
+        , message: data.message
+        }, function() {
+          if (data.extras.url) {
+            Backbone.history.navigate(data.extras.url, { trigger: true });
+          }
+        });
+      }
+    });
+
+    // enable push
+    push.enablePush();
+
+    // enable location
+    push.enableLocation();
+
+    // enable background location
+    push.enableBackgroundLocation();
+
+    // enable auto badge
+    push.setAutobadgeEnabled(true, function() {});
+
+    // set the alias to their id
+    push.setAlias(id, function() {});
+    
+    // set the tags so we can find them
+    push.setTags(tags, function() {});
+
+    // Register for sound, alert, and badge push notifications
+    push.registerForNotificationTypes(push.notificationType.sound | push.notificationType.alert | push.notificationType.badge);
+  }
+}
+
+/**
+ * Disable push
+ */
+
+function disable() {
+  if (push) {
+    push.resetBadge(function() {});
+    push.disablePush();
+    push.disableLocation();
+    push.disableBackgroundLocation();
+  }
+}
