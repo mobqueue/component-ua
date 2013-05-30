dee3f474949664b1e3b2e74180d63262af8a61ed
Trevor Gerhardt <trevorgerhardt@gmail.com>
2013-05-29 20:00:14 -0400

    Added callback

--- index.js
+++ index.js
@@ -28,73 +28,66 @@ module.exports.disable = disable;
  * Register device id and tags
  */
 
-function enable(id, tags) {
+function enable(id, tags, callback) {
+  callback = callback || function(){};
+
   // if you have push...
   if (push) {
-    // disable push first
-    disable();
-    // on register
-    push.registerEvent('registration', function(id) {
-      // check if push is enabled
-      push.isPushEnabled(function(enabled) {
-        if (!enabled) {
-          cordova.alert({
-            title: 'Push Notifications'
-          , message: 'Push notifications must be enabled for this app to work. Please check your settings.'
-          }, function() {
-            setTimeout(function() {
-              enable();
-            }, 1000);
-          });
-        }
-      });
-    });
-          
-    push.registerEvent('push', function(data) {
-      cordova.alert({
-        title: data.extras.title || 'Perfect'
-      , message: data.message
-      }, function() {
-        if (data.extras.url) {
-          Backbone.history.navigate(data.extras.url, { trigger: true });
-        }
-      });
-    });
-
-    push.getIncoming(function(data) {
-      if (data.message) {
-        cordova.alert({
-          title: data.extras.title || 'Perfect'
-        , message: data.message
-        }, function() {
-          if (data.extras.url) {
-            Backbone.history.navigate(data.extras.url, { trigger: true });
-          }
-        });
+    // check if push is enabled
+    push.isPushEnabled(function(enabled) {
+      if (enabled) {
+        callback();
+      } else {
+        register(id, tags, callback);
       }
     });
-
-    // enable push
-    push.enablePush();
-
-    // enable location
-    push.enableLocation();
-
-    // enable background location
-    push.enableBackgroundLocation();
-
-    // enable auto badge
-    push.setAutobadgeEnabled(true, function() {});
-
-    // set the alias to their id
-    push.setAlias(id, function() {});
-    
-    // set the tags so we can find them
-    push.setTags(tags, function() {});
-
-    // Register for sound, alert, and badge push notifications
-    push.registerForNotificationTypes(push.notificationType.sound | push.notificationType.alert | push.notificationType.badge);
+  } else {
+    callback();
   }
+}
+
+/**
+ * Register
+ */
+
+function register(id, tags, callback) {
+  // on register
+  push.registerEvent('registration', callback);
+  
+  // push
+  push.registerEvent('push', function(data) {
+    cordova.alert({
+      title: data.extras.title || 'Perfect'
+    , message: data.message
+    });
+  });
+
+  push.getIncoming(function(data) {
+    if (data.extras.url) {
+      Backbone.history.navigate(data.extras.url, { trigger: true });
+    }
+  });
+
+  // enable push
+  push.enablePush();
+
+  // enable location
+  push.enableLocation();
+
+  // enable background location
+  push.enableBackgroundLocation();
+
+  // enable auto badge
+  push.setAutobadgeEnabled(true);
+
+  // set the alias to their id
+  push.setAlias(id);
+  
+  // set the tags so we can find them
+  push.setTags(tags);
+
+  // Register for sound, alert, and badge push notifications
+  push.registerForNotificationTypes(push.notificationType.sound | push.notificationType.alert | push.notificationType.badge);
 }
 
 /**
@@ -103,7 +96,7 @@ function enable(id, tags) {
 
 function disable() {
   if (push) {
-    push.resetBadge(function() {});
+    push.resetBadge();
     push.disablePush();
     push.disableLocation();
     push.disableBackgroundLocation();
